634488a818fc66c9fa53eca7898b3c8b2adf4ffe
laurentfaggin <laurentfaggin@hotmail.fr>
2023-08-10 08:37:04 -0400

    listeChainee et node generiques

--- Module09_ListeChainee/listeChaineeGenerique.h
+++ Module09_ListeChainee/listeChaineeGenerique.h
@@ -73,7 +73,173 @@ public:
 			if (this->m_nombreMaillons == 0) {
 				std::invalid_argument("la liste est vide");
 			}
-			noeudPrecedent = noeudPrecedent->m
+			noeudPrecedent = noeudPrecedent->Next();
+			--compteur;
+		}
+		return noeudPrecedent;
+	}
+
+	Node<TypeElement>* nodeCourant(const int& p_indice) {
+		Node<TypeElement>* noeudCourant = this->m_debut;
+		int compteur = p_indice;
+		while (compteur != 0) {
+			noeudCourant = noeudCourant->m_suivant;
+			--compteur;
+		}
+		return noeudCourant;
+	}
+
+	void ajouterFin(const int& p_indice) {
+		Node<TypeElement>* noeudFin = new Node(p_valeur);
+		if (this->m_nombreMaillons == 0) {
+			this->m_debut = noeudFin;
+			this->m_fin = noeudFin;
+		}
+		else {
+			Node<TypeElement>* futurPrecedent = this->nodePrecedent(this->m_nombreMaillons);
+			noeudFin->modifierSuivant(futurPrecedent->m_suivant);
+			futurPrecedent->modifierSuivant(noeudFin);
+			this->m_fin = noeudFin;
+		}
+		++this->m_nombreMaillons;
+	}
+
+	void inserer(const int& p_valeur, const int& p_indice) {
+		Node<TypeElement>* newNode = new Node(p_valeur);
+		if (p_indice > this->m_nombreMaillons) {
+			std::invalid_argument("valeur d'indice trop elevee");
+		}
+		else if (this->m_nombreMaillons != 0) {
+			Node<TypeElement>* futurNoeudPrecedent = this->nodePrecedent(p_indice);
+			newNode->modifierSuivant(futurNoeudPrecedent->m_suivant);
+			futurNoeudPrecedent->modifierSuivant(newNode);
+		}
+		else {
+			this->ajouterDebut(newNode->obtenirDonnee());
+		}
+	}
+
+	void supprimerDebut() {
+		if (this->m_nombreMaillons == 0) {
+			std::invalid_argument("la liste est vide");
+		}
+		else if (this->m_nombreMaillons == 1) {
+			this->m_debut = nullptr;
+			this->m_fin = nullptr;
+			this->m_nombreMaillons = 0;
+		}
+		else {
+			Node<TypeElement>* noeudAEffacer = this->m_debut;
+			this->m_debut = this->m_debut->m_suivant;
+			noeudAEffacer->m_suivant = nullptr;
+			delete noeudAEffacer;
+			--this->m_nombreMaillons;
+		}
+	}
+
+	void supprimerFin() {
+		int compteur = this->m_nombreMaillons;
+		if (this->m_nombreMaillons == 0) {
+			std::invalid_argument("la liste est vide");
+		}
+		else if (this->m_nombreMaillons == 1) {
+			Node<TypeElement>* noeudAEffacer = this->m_debut;
+			this->m_debut = this->m_fin = nullptr;
+			delete this->m_fin;
+			delete noeudAEffacer;
+		}
+		else {
+			Node<TypeElement>* futurDernier = this->nodePrecedent(this->m_nombreMaillons);
+			delete futurDernier->m_suivant;
+			futurDernier->m_suivant = nullptr;
+			this->m_fin = futurDernier;
+			--m_nombreMaillons;
+		}
+	}
+
+	void supprimerA(const int& p_indice) {
+		if (p_indice >= 0 && p_indice < this->m_nombreMaillons) {
+			if (p_indice == 0) {
+				this->supprimerDebut();
+			}
+			else {
+				Node<TypeElement>* noeudPrecedent = this->nodePrecedent(p_indice);
+				Node<TypeElement>* noeudAEffacer = this->nodeCourant(p_indice);
+				noeudPrecedent->modifierSuivant(noeudAEffacer->m_suivant);
+				noeudAEffacer->m_suivant = nullptr;
+			}
+			--m_nombreMaillons;
+		}
+		else {
+			std::invalid_argument("l'indice n'est pas correct ou  la liste est vide");
+		}
+	}
+
+	void vider() {
+		while (this->m_nombreMaillons > 0) {
+			this->supprimerDebut();
+		}
+	}
+
+	TypeElement obtenir(const int& p_indice) {
+		if (this->m_nombreMaillons == 0 || p_indice >= this->m_nombreMaillons) {
+			std::invalid_argument("aucun element a la position donnee");
+		}
+		else {
+			Node<TypeElement>* elementCherche = this->nodeCourant(p_indice);
+			return elementCherche->obtenirDonnees();
+		}
+	}
+
+	int nombreMaillons() {
+		return this->m_nombreMaillons;
+	}
+
+	ListeChainee& operator = (ListeChainee&& p_listeADeplacer) {
+		if (this != &p_listeADeplacer) {
+			this->vider();
+			this->m_debut = p_listeADeplacer.m_debut;
+			this->m_fin = p_listeADeplacer.m_fin;
+			this->m_nombreMaillons = p_listeADeplacer.m_nombreMaillons;
+
+			this->m_debut = nullptr;
+			this->m_fin = nullptr;
+			this->m_nombreMaillons = 0;
+		}
+		return *this;
+	}
+
+	void parcourir(void(*p_fonction)(const int&)) {
+		if (this->m_nombreMaillons == 0) {
+			std::invalid_argument("la liste est vide");
+		}
+		else {
+			Node<TypeElement>* noeudCourant = this->m_debut;
+			while (noeudCourant != nullptr) {
+				p_fonction(noeudCourant->obtenirDonnee());
+				noeudCourant = noeudCourant->m_suivant;
+			}
+		}
+	}
+
+	void trier(bool(*p_fonctionTri)(const int&, const int&)) {
+		if (this->m_nombreMaillons == 0) {
+			std::invalid_argument("la liste est vide");
+		}
+		else {
+			Node<TypeElement>* noeudCourant = this->m_debut;
+			while (noeudCourant != nullptr) {
+				Node<TypeElement>* noeudSuivant = noeudCourant->m_suivant;
+				while (noeudSuivant != nullptr) {
+					if (!p_fonctionTri(noeudCourant->obtenirDonnee(), noeudSuivant->obtenirDonnee())) {
+						int temp = noeudCourant->obtenirDonnee();
+						noeudCourant->modifierDonnees(noeudSuivant->obtenirDonnee());
+						noeudSuivant->modifierDonnees(temp);
+					}
+					noeudSuivant = noeudSuivant->m_suivant;
+				}
+				noeudCourant = noeudCourant->m_suivant;
+			}
 		}
 	}
 
